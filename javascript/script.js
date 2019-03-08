c48944265373c2539834cab491cc5f1300a94a83
SheriffHobo <zombiemaster@gmail.com>
2019-03-08 12:47:14 -0800

    table refresh, not DOM reload

--- javascript/script.js
+++ javascript/script.js
@@ -1,48 +1,50 @@
 var config = {
-    apiKey: "AIzaSyARoZlhBsxEOy5FG73z7V25UH4R3tpfr6I",
-    authDomain: "trainschedulehomework-ddf3b.firebaseapp.com",
-    databaseURL: "https://trainschedulehomework-ddf3b.firebaseio.com",
-    projectId: "trainschedulehomework-ddf3b",
-    storageBucket: "trainschedulehomework-ddf3b.appspot.com",
-    messagingSenderId: "892044864535"
+  apiKey: "AIzaSyARoZlhBsxEOy5FG73z7V25UH4R3tpfr6I",
+  authDomain: "trainschedulehomework-ddf3b.firebaseapp.com",
+  databaseURL: "https://trainschedulehomework-ddf3b.firebaseio.com",
+  projectId: "trainschedulehomework-ddf3b",
+  storageBucket: "trainschedulehomework-ddf3b.appspot.com",
+  messagingSenderId: "892044864535"
 };
 firebase.initializeApp(config);
-
 var database = firebase.database();
 
 function currentTime() {
-    var current = moment().format('HH:mm');
-    $(".time").text(current);
-    setTimeout(currentTime, 1000);
+  var current = moment().format('HH:mm');
+  $(".time").text(current);
+  setTimeout(currentTime, 1000);
+};
+
+$("#add-train-btn").on("click", function (event) {
+  event.preventDefault();
+
+  var train = $("#traininput").val().trim();
+  var destination = $("#destinput").val().trim();
+  var time = $("#timeinput").val().trim();
+  var frequency = $("#freqinput").val().trim();
+
+  var newTrain = {
+    type: "train",
+    train: train,
+    destination: destination,
+    time: time,
+    frequency: frequency
   };
+
+  database.ref().push(newTrain);
+
+  $("#train").val("");
+  $("#destinput").val("");
+  $("#timeinout").val("");
+  $("#freqinput").val("");
+});
+
+function setPage() {
+  $("#schedtable").empty();
 
   currentTime();
 
-$("#add-train-btn").on("click", function (event) {
-    event.preventDefault();
-
-    var train = $("#traininput").val().trim();
-    var destination = $("#destinput").val().trim();
-    var time = $("#timeinput").val().trim();
-    var frequency = $("#freqinput").val().trim();
-
-    var newTrain = {
-        type: "train",
-        train: train,
-        destination: destination,
-        time: time,
-        frequency: frequency
-    };
-
-    database.ref().push(newTrain);
-
-    $("#train").val("");
-    $("#destinput").val("");
-    $("#timeinout").val("");
-    $("#freqinput").val("");
-});
-
-database.ref().on("child_added", function (childSnapshot) {
+  database.ref().on("child_added", function (childSnapshot) {
     // console.log(childSnapshot.val());
 
     var trainName = childSnapshot.val().train;
@@ -69,23 +71,26 @@ database.ref().on("child_added", function (childSnapshot) {
     // console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
 
     var newRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(trainDest),
-        $("<td>").text(trainFreq),
-        $("<td>").text(moment(nextArrival).format("HH:mm")).attr("id", "refresh"),
-        $("<td>").text(tMinutesTillTrain).attr("id", "refresh"),
-        $("<td>").text("X").attr("data-value", key).attr("class", "delete btn btn-sm btn-outline-danger")
+      $("<td>").text(trainName),
+      $("<td>").text(trainDest),
+      $("<td>").text(trainFreq),
+      $("<td>").text(moment(nextArrival).format("HH:mm")).attr("id", "refresh"),
+      $("<td>").text(tMinutesTillTrain).attr("id", "refresh"),
+      $("<td>").text("X").attr("data-value", key).attr("class", "delete btn btn-sm btn-outline-danger")
     );
 
     $("#schedtable").append(newRow);
+  });
+};
+
+setPage();
+
+$(document).on("click", ".delete", function () {
+  keyref = $(this).attr("data-value");
+  database.ref().child(keyref).remove();
+  setPage();
 });
 
-$(document).on("click", ".delete", function() {
-    keyref = $(this).attr("data-value");
-    database.ref().child(keyref).remove();
-    location.reload();
-  });
-
-  setInterval(function() {
-    location.reload();
-  }, 60000);
+setInterval(function () {
+  setPage();
+}, 60000);
